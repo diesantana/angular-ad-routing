2ff6ae294c7abe8c1d70ea4236662417c3a64b09
Diego Santana <3c.santana@gmail.com>
2024-06-24 08:20:52 -0300

    feat: Implementa o componente de login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Usuario } from './usuario';
+import { AuthService } from './auth.service';
 
 @Component({
   selector: 'app-login',
@@ -6,5 +8,12 @@ import { Component } from '@angular/core';
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  usuario: Usuario = new Usuario();
 
+  constructor(private authService: AuthService) { }
+
+  fazerLogin(){
+    // console.log(this.usuario);
+    this.authService.fazerLogin(this.usuario);
+  }
 }
