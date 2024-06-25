b92551ab7d0997602940784908790b2bbe9be30d
Diego Santana <3c.santana@gmail.com>
2024-06-25 18:07:45 -0300

    feat: Aula CanMatch Guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,18 +2,22 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
-import { authGuard } from './guards/auth.guard';
+import { authGuard, canMatchGuard } from './guards/auth.guard';
 
 const routes: Routes = [
   {
     path: 'cursos',
     loadChildren: () => import('./cursos/curso.module').then(m => m.CursoModule),
-    canActivate: [authGuard]
+    canActivate: [authGuard],
+    // canMatch: [authGuard]
+    canMatch: [canMatchGuard]
   },
   {
     path: 'alunos',
     loadChildren: () => import('./alunos/alunos.module').then(m => m.AlunosModule),
-    canActivate: [authGuard]
+    canActivate: [authGuard],
+    // canMatch: [authGuard]
+    canMatch: [canMatchGuard]
   },
   {path: '', component: HomeComponent, canActivate: [authGuard]},
   {path: 'login', component: LoginComponent}
--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -1,5 +1,5 @@
 import { inject } from '@angular/core';
-import { ActivatedRoute, CanActivateFn, Router } from '@angular/router';
+import { CanActivateFn, CanMatchFn, Route, Router, UrlSegment } from '@angular/router';
 import { AuthService } from '../login/auth.service';
 
 export const authGuard: CanActivateFn = (route, state) => {
@@ -9,6 +9,20 @@ export const authGuard: CanActivateFn = (route, state) => {
   if(auth.verificaUsuarioAutenticado) {
     return true;
   }
+  router.navigate(['/login']);
+  return false;
+};
+
+export const canMatchGuard: CanMatchFn = (route: Route, segments: UrlSegment[]) => {
+  const auth = inject(AuthService);
+  const router = inject(Router);
+
+  if(auth.verificaUsuarioAutenticado) {
+    console.log("Modulo liberado! 💂💂 ");
+    return true;
+
+  }
+  console.log("Sem acesso ao modulo! 💂💂 ");
   router.navigate(['/login']);
   return false;
 };
