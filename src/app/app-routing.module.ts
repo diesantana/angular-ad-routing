72d25928428d7bd90d158653580346504c7e8b82
Diego Santana <3c.santana@gmail.com>
2024-06-25 15:43:11 -0300

    feat: Aula canActivate e canDeactivate

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,17 +2,20 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
+import { authGuard } from './guards/auth.guard';
 
 const routes: Routes = [
   {
     path: 'cursos',
-    loadChildren: () => import('./cursos/curso.module').then(m => m.CursoModule)
+    loadChildren: () => import('./cursos/curso.module').then(m => m.CursoModule),
+    canActivate: [authGuard]
   },
   {
     path: 'alunos',
-    loadChildren: () => import('./alunos/alunos.module').then(m => m.AlunosModule)
+    loadChildren: () => import('./alunos/alunos.module').then(m => m.AlunosModule),
+    canActivate: [authGuard]
   },
-  {path: '', component: HomeComponent},
+  {path: '', component: HomeComponent, canActivate: [authGuard]},
   {path: 'login', component: LoginComponent}
 ];
 
